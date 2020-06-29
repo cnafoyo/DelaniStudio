34cd5dfa5e9a002620a32c10d11e0f870485a597
Billy Chepkemoi <cnafayo@gmail.com>
2020-06-29 15:18:26 +0300

    editing of 'what we do'

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-$(document).ready(function() {
-  $("img").click(function() {
-    $(".content").toggle(token:250);
-    $(".image").toggle(token:250);
+$(document).ready(function () {
+  $("#design-text").hide();
+  $("#design").click(function () {
+    $("#design-text").slideToggle("slow");
+    $("#design-img").slideToggle("slow");
   });
-});
+  $("#development-text").hide();
+  $("#development").click(function () {
+    $("#development-text").slideToggle("slow");
+    $("#development-img").slideToggle("slow");
+  });
+  $("#product-text").hide();
+  $("#product").click(function () {
+    $("#product-text").slideToggle("slow");
+    $("#product-img").slideToggle("slow");
+  });
+  $(".portfolio").each(function () {
+    $(this).find("p").hide()
+    $(this).animate({ opacity: 1 });
+    $(this).hover(function () {
+      $(this).stop().animate({ opacity: .4 }, 200);
+      $(this).find("p").fadeIn();
+    }, function () {
+      $(this).stop().animate({ opacity: 1 }, 400)
+      $(this).find("p").fadeOut();
+    });
+  });
+}); 
